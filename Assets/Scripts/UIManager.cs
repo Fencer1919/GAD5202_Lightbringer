1f1b8d55fe9548c1f81adaad261a53738e5c5453
ardagenc <genc.arda@gmail.com>
2024-05-26 22:11:16 +0300

    Presentation Version

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,23 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance;
 
     public GameObject AttributeTab;
-
     public Button[] attributeButtons;
 
     public Slider healthBar;
+
+    public TMP_Text oathBoundText;
+    public TMP_Text oathBreakerText;
+
+    public TMP_Text levelText;
+
+    public TMP_Text strText;
+    public TMP_Text dexText;
+    public TMP_Text hpText;
+    public TMP_Text wisText;
+    public TMP_Text intText;
+    public TMP_Text hsText;
+
+
 
     private void Awake()
     {
@@ -25,13 +39,30 @@ public class UIManager : MonoBehaviour
         LevelSystem.onLevelUp += OnLevelUp;
     }
 
+    private void Start()
+    {
+        levelText.text = "Level: 0";
+    }
+
+    public void SetAttribute(TMP_Text attributeText, int attributeValue)
+    {
+        attributeText.text = attributeValue.ToString();
+    }
+
+    public void SetOathValue(TMP_Text text, String s,int i)
+    {
+        text.text = s + i.ToString();
+    }
+
     public void SetHealthBar(float healthAmount)
     {
         healthBar.value = healthAmount;
     }
 
-    private void OnLevelUp()
+    private void OnLevelUp(int i)
     {
+        levelText.text = "Level: " + i.ToString();
+
         foreach (Button button in attributeButtons)
         {
             button.interactable = true;
